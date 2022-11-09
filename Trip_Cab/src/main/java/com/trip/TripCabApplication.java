8da517da78634f62e2c7c7db9282c21efb94b784
VINAYKUMARKUNDER <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2022-11-09 16:16:34 +0530

    projoct - model

---
+++ Trip_Cab/src/main/java/com/trip/TripCabApplication.java
@@ -0,0 +1,13 @@
+package com.trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripCabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripCabApplication.class, args);
+	}
+
+}
